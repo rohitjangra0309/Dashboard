4028e268eb08b3abafc8766a90718319e27a52a1
Rohit Jangra <rohit19444@iiitd.ac.in>
2025-06-30 21:48:11 +0530

    saving progress, will come back later to charts

--- dashboard/src/Components/Charts/Charts.jsx
+++ dashboard/src/Components/Charts/Charts.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, Area, AreaChart } from 'recharts'
-import './Charts.css'
+import '../../App.css'
 
 const Charts = ({ brandContentData, assessmentData }) => {
     return (
